3b8b6510c7ab1f39e90b92b4d8d9cd012a5178be
Rahul verma <rahulverma206122@gmail.com>
2025-09-02 19:28:46 +0530

    Updated header components

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -42,7 +42,7 @@ function AuthLogin() {
         <p className="mt-2">
           Don't have an account
           <Link
-            className="font-medium text-blue-600 ml-2 text-primary hover:underline"
+            className="font-medium text-blue-600 ml-2 px-4 text-primary hover:underline"
             to="/auth/register"
           >
             Register
